ec71c62f1d527744a60e0d11b334df6674705cd3
iamsadhik <76804961+iamsadhik@users.noreply.github.com>
2021-01-26 19:56:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,23 +2,50 @@ import React, { useState } from "react";
 import "./styles.css";
 var heading = "sadhik";
 var color = "red";
-var likeCounter = 0;
+var db = {
+  "🚣": "Person Rowing Boa",
+  "🏕️": "Camping",
+  "🏞️": "National Park",
+  "🏝️": "Desert Island",
+  "🌇": "Sunset",
+  "🚅": "Bullet Train"
+};
 
+var database = Object.keys(db);
 export default function App() {
-  var [like, setlike] = useState(0);
-  function clickhandler() {
-    console.log("clicked", likeCounter);
-    likeCounter = likeCounter + 1;
-    setlike(likeCounter);
+  var [type, settype] = useState();
+
+  function clickHandler(vijay) {
+    var output = db[vijay];
+    settype(output);
   }
+  function inputHandler(message) {
+    var userInput = message.target.value;
+    var output = db[userInput];
+    settype(output);
+  }
+
   return (
     <div className="App">
       <h1>
         welcome <span style={{ color: color }}>{heading}</span>
       </h1>
-      <input></input>
-      <button onClick={clickhandler}>like</button>
-      {like}
+      <input onChange={inputHandler}></input>
+      <h3>{type}</h3>
+      <h2>emojis we know:</h2>
+      <div>
+        {database.map(function (vijay) {
+          return (
+            <span
+              onClick={() => clickHandler(vijay)}
+              key={vijay}
+              style={{ padding: "0.5rem", cursor: "pointer" }}
+            >
+              {vijay}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
